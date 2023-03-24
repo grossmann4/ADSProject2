626a741651bea9b8a0cf68a51ea83efc5b873fe8
grossmann4 <grossmann4@yahoo.com>
2023-03-24 17:20:05 -0400

    testing part3

--- main.py
+++ main.py
@@ -44,6 +44,9 @@ def get_website(url):
     for script in soup(["script", "style"]):
         script.extract()
     text = soup.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
     return text
 
 def main():
