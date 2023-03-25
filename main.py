19e568c31ffb9994bc2e07530fcfd021aaaf6bb7
grossmann4 <grossmann4@yahoo.com>
2023-03-25 16:25:08 -0400

    testing part3

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ import spanbert as span
 import spacy_help_functions as sp
 from googleapiclient.discovery import build
 
+# max webpages
 MAX_ITEMS = 10
+# entities of interest
 ENTS = ['ORG', 'PERSON', 'LOC', 'GPE', 'DATE']
 
+# searches google and returns the top 10 webpages
 def get_google_search_items(api_key, engine_id, words):
     service = build(
         "customsearch", "v1", developerKey=api_key
@@ -31,11 +34,13 @@ def get_google_search_items(api_key, engine_id, words):
     items = res['items'][:MAX_ITEMS]
     return items
 
+# formats the webpages into dictionary
 def get_formatted_items(items):
     def get_attr(item):
         return {'title': item['title'], 'url': item['link'], 'description': item['snippet']}
     return [get_attr(i) for i in items]
 
+# scrape the webpage and return the plaintext
 def get_website(url):
     # try to open the url
     try:
@@ -54,7 +59,43 @@ def get_website(url):
     # maybe look into taking off first few chars of text if its unneccessary
     return text
 
-def extract(text, R, T):
+def run_spanbert(examples):
+    # Initialize return dictionary
+    res = defaultdict(int)
+    # Load spanbert model
+    spanbert_model = span.SpanBERT("./pretrained_spanbert")
+
+    preds = spanbert_model.predict(examples)
+
+    # taken from spacy_help_functions
+    # retrieves spanbert entity pairs
+    for ex, pred in list(zip(examples, preds)):
+        relation = pred[0]
+        if relation == 'no_relation':
+            continue
+        print("\n\t\t=== Extracted Relation ===")
+        print("\t\tTokens: {}".format(ex['tokens']))
+        subj = ex["subj"][0]
+        obj = ex["obj"][0]
+        confidence = pred[1]
+        print("\t\tRelation: {} (Confidence: {:.3f})\nSubject: {}\tObject: {}".format(relation, confidence, subj, obj))
+        if confidence > T:
+            if res[(subj, relation, obj)] < confidence:
+                res[(subj, relation, obj)] = confidence
+                print("\t\tAdding to set of extracted relations")
+            else:
+                print("\t\tDuplicate with lower confidence than existing record. Ignoring this.")
+        else:
+            print("\t\tConfidence is lower than threshold confidence. Ignoring this.")
+        print("\t\t==========")
+    return res
+
+# TODO
+def run_gpt3(sentence):
+    return 0
+
+# extract the relations for a webpage via spanbert
+def extract(text, R, T, OPTION):
     res = defaultdict(int)
     # Set entities of interest
     if R in [1, 2, 4]:
@@ -64,17 +105,17 @@ def extract(text, R, T):
 
     # Initialize spacy
     nlp = spacy.load("en_core_web_lg")
-    # Load spanbert model
-    spanbert_model = span.SpanBERT("./pretrained_spanbert")
-
+    
     # Split plaintext into sentences and extract relations
-    # doc = nlp(text)
-    doc = nlp("Bill Gates stepped down as chairman of Microsoft in February 2014 and assumed a new post as technology adviser to support the newly appointed CEO Satya Nadella.")
+    doc = nlp(text)
+
+    # doc = nlp("Bill Gates stepped down as chairman of Microsoft in February 2014 and assumed a new post as technology adviser to support the newly appointed CEO Satya Nadella.")
 
     for sent in doc.sents:
         # create entity pairs for each sentence
         ents = sp.create_entity_pairs(sent, entities_of_interest)
         # print("ENTS: {}".format(ents))
+
         # if there are no named entities, go to next sentence
         if not ents:
             continue
@@ -97,38 +138,22 @@ def extract(text, R, T):
                     examples.append({"tokens": ep[0], "subj": ep[1], "obj": ep[2]})
                 elif ep[2][1] == 'ORGANIZATION' and ep[1][1] == 'PERSON':
                     examples.append({"tokens": ep[0], "subj": ep[2], "obj": ep[1]})
+
         # print("EXAMPLES: {}".format(examples))
-        # run spanbert if there are relevant entity pairs
+
+        # if there are examples, find relations
         if examples:
-            preds = spanbert_model.predict(examples)
-            print('here')
-            print(examples)
+            if OPTION == '-spanbert':
+                r = run_spanbert(examples)
+                res.update(r)
+            else:
+                run_gpt3(sent.text)
+            
+            # print('here')
+            # print(examples)
         # otherwise go to next sentence
         else:
             continue
-
-        # taken from spacy_help_functions
-        # retrieves spanbert entity pairs
-        for ex, pred in list(zip(examples, preds)):
-            relation = pred[0]
-            if relation == 'no_relation':
-                continue
-            print("\n\t\t=== Extracted Relation ===")
-            print("\t\tTokens: {}".format(ex['tokens']))
-            subj = ex["subj"][0]
-            obj = ex["obj"][0]
-            confidence = pred[1]
-            print("\t\tRelation: {} (Confidence: {:.3f})\nSubject: {}\tObject: {}".format(relation, confidence, subj, obj))
-            if confidence > T:
-                if res[(subj, relation, obj)] < confidence:
-                    res[(subj, relation, obj)] = confidence
-                    print("\t\tAdding to set of extracted relations")
-                else:
-                    print("\t\tDuplicate with lower confidence than existing record. Ignoring this.")
-            else:
-                print("\t\tConfidence is lower than threshold confidence. Ignoring this.")
-            print("\t\t==========")
-
     return res
 
 def main():
@@ -144,7 +169,7 @@ def main():
     GOOGLE_API_KEY = sys.argv[2]
     GOOGLE_ENGINE_ID = sys.argv[3]
     OPEN_AI_KEY = sys.argv[4]
-    R = float(sys.argv[5])
+    R = int(sys.argv[5])
     if R not in [1, 2, 3, 4]:
         print('r has to be an integer between 1 and 4')
         return 0
@@ -154,7 +179,7 @@ def main():
         return 0
     Q = sys.argv[7].split()
     K = int(sys.argv[8])
-    if K < 0:
+    if K <= 0:
         print('k has to be greater than 0')
         return 0
     
@@ -181,14 +206,15 @@ def main():
             # add url to URLS and get 10000 chars of text
             URLS.append(url)
             plaintext = get_website(url)
+            # if url cannot be opened, go to next one
             if plaintext == 0:
                 continue
             if len(plaintext) > 10000:
                 plaintext = plaintext[:10000]
             # split text into sentences and extract entities
-            relations = extract(plaintext, R, T)
+            relations = extract(plaintext, R, T, OPTION)
             print(relations)
-            break
+            
     return 0
 
 if __name__ == "__main__":
