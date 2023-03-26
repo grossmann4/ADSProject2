794d20ebeea1145247562b536c545089ecfbae08
grossmann4 <grossmann4@yahoo.com>
2023-03-26 15:51:11 -0400

    testing scrape

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def get_website(url):
     soup = bs4.BeautifulSoup(html, 'html.parser')
     for script in soup(["script", "style"]):
         script.extract()
-    text = soup.get_text()
+    text = soup.body.get_text()
     lines = (line.strip() for line in text.splitlines())
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     text = '\n'.join(chunk for chunk in chunks if chunk)
