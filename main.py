dac9d496c78fed3d1337ca0d83b192a0de4dff25
mbu13 <matthewbu13@gmail.com>
2023-03-26 19:38:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,30 @@ import spacy
 import spanbert as span
 import spacy_help_functions as sp
 from googleapiclient.discovery import build
+import os
+import openai
+import re
+import ast
+import time
 
 # max webpages
 MAX_ITEMS = 10
 # entities of interest
 ENTS = ['ORG', 'PERSON', 'LOC', 'GPE', 'DATE']
+
+RELATION_TEXT_MAPPING = {
+    'Schools_Attended' : 'per:schools_attended',
+    'Work_For' : 'per:employee_of',
+    'Live_In' : 'per:cities_of_residence',
+    'Top_Member_Employees' : 'org:top_members/employees'
+}
+
+R_INT_MAPPING = {
+    1 : 'Schools_Attended',
+    2 : 'Work_For',
+    3 : 'Live_In',
+    4 : 'Top_Member_Employees'
+}
 
 # searches google and returns the top 10 webpages
 def get_google_search_items(api_key, engine_id, words):
@@ -84,11 +103,63 @@ def run_spanbert(examples, T, spanbert_model):
     return res
 
 # TODO
-def run_gpt3(sentence):
-    return 0
+def get_openai_completion(prompt, model, max_tokens, temperature = 0.2, top_p = 1, frequency_penalty = 0, presence_penalty =0):
+    response = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        max_tokens=max_tokens,
+        temperature=temperature,
+        top_p=top_p,
+        frequency_penalty=frequency_penalty,
+        presence_penalty=presence_penalty
+    )
+    response_text = response['choices'][0]['text']
+    return response_text
+
+def run_gpt3(sentence, valid_relation):
+
+    prompt_text = '''
+                    Given a sentence, extract all the valid relations between nouns in tuple format (subject, relation, object).
+                    If the relation is Schools_Attended, use format (person, Schools_Attended, institution).
+                    If the relation is Work_For, use format (employee, Work_For, organization).
+                    If the relation is Live_In, use format (resident, Live_In, place).
+                    If the relation is Top_Member_Employees, use format (employee, Top_Member_Employees, organization).
+                    Avoid generic pronouns.
+                    valid relations: {}
+                    sentence: {}
+                    extracted: 
+                    '''.format(valid_relation, sentence)
+
+    model = 'text-davinci-003'
+    max_tokens = 100
+    temperature = 0.2
+    top_p = 1
+    frequency_penalty = 0
+    presence_penalty = 0
+
+    response_text = get_openai_completion(prompt_text, model, max_tokens, temperature, top_p, frequency_penalty, presence_penalty)
+    
+    # (Bill Gates, Schools_Attended), (Bill Gates, Work_For, Microsoft), (John Smith, Work_For, Microsoft), (Bill Gates, Top_Member_Employees, Microsoft), (John Smith, Top_Member_Employees, Microsoft)
+    tuple_strs = re.split(r',\s*(?![^()]*\))', response_text)
+    tuples = [tuple(s.replace('(', '').replace(')', '').split(',')) for s in tuple_strs]
+
+    result = {}
+
+    # transform data to output
+    for t in tuples:
+        if len(t) < 3:
+            continue
+        if not RELATION_TEXT_MAPPING.get(t[1].strip()):
+            continue
+
+        relation = RELATION_TEXT_MAPPING.get(t[1].strip())
+        new_tup = (t[0].strip(), relation, t[2].strip())
+        result[new_tup] = 1.0
+
+    return result
 
 # extract the relations for a webpage via spanbert
-def extract(text, R, T, OPTION, spanbert_model, nlp):
+def extract(text, R, T, OPTION, spanbert_model, nlp, valid_relation):
     res = defaultdict(int)
     # Set entities of interest
     if R in [1, 2, 4]:
@@ -136,7 +207,9 @@ def extract(text, R, T, OPTION, spanbert_model, nlp):
                 r = run_spanbert(examples, T, spanbert_model)
                 res.update(r)
             else:
-                run_gpt3(sent.text)
+                r = run_gpt3(sent.text, valid_relation)
+                res.update(r)
+                time.sleep(1.5)
             
             # print('here')
             # print(examples)
@@ -159,6 +232,7 @@ def main():
     GOOGLE_API_KEY = sys.argv[2]
     GOOGLE_ENGINE_ID = sys.argv[3]
     OPEN_AI_KEY = sys.argv[4]
+    openai.api_key = OPEN_AI_KEY
     R = int(sys.argv[5])
     if R not in [1, 2, 3, 4]:
         print('r has to be an integer between 1 and 4')
@@ -216,7 +290,7 @@ def main():
             if len(plaintext) > 10000:
                 plaintext = plaintext[:10000]
             # split text into sentences and extract entities
-            relations = extract(plaintext, R, T, OPTION, spanbert_model, nlp)
+            relations = extract(plaintext, R, T, OPTION, spanbert_model, nlp, R_INT_MAPPING.get(R))
             res.update(relations)
             # print('3')
     print(res)
