bb6b09153ea23677872b044f86ce21362f7f9298
mbu13 <matthewbu13@gmail.com>
2023-03-26 20:57:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import sys
-import json
-import math
 import urllib.request
 import bs4
 import sys
@@ -9,10 +7,8 @@ import spacy
 import spanbert as span
 import spacy_help_functions as sp
 from googleapiclient.discovery import build
-import os
 import openai
 import re
-import ast
 import time
 
 # max webpages
@@ -61,33 +57,25 @@ def get_formatted_items(items):
 
 # scrape the webpage and return the plaintext
 def get_website(url):
-    text = ""
     # try to open the url
     try:
         r = urllib.request.urlopen(url)
         html = r.read()
     except urllib.error.HTTPError as e:
-        print('bad')
+        print('Website could not be opened, moving on to next one.\n')
         return 0
     # extract text and clean up newline/spaces
     soup = bs4.BeautifulSoup(html, 'html.parser')
-
-    for para in soup.find_all('p'):
-        t = para.get_text()
-        text += t
-
-    if not text:
-        # print(text)
-        # print('BADDDDD')
-        for script in soup(["script", "style"]):
-            script.extract()
-        text = soup.body.get_text()
-        lines = (line.strip() for line in text.splitlines())
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        text = '\n'.join(chunk for chunk in chunks if chunk)
+    for script in soup(["script", "style"]):
+        script.extract()
+    text = soup.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    # maybe look into taking off first few chars of text if its unneccessary
     return text
 
-def run_spanbert(examples, T, spanbert_model):
+def run_spanbert(examples, T, spanbert_model, R):
     # Initialize return dictionary
     res = defaultdict(int)
 
@@ -95,21 +83,29 @@ def run_spanbert(examples, T, spanbert_model):
 
     # taken from spacy_help_functions
     # retrieves spanbert entity pairs
-    for ex, pred in list(zip(examples, preds)):
+    l = list(zip(examples, preds))
+    for ex, pred in l:
         relation = pred[0]
         if relation == 'no_relation':
             continue
-        # print("\n\t\t=== Extracted Relation ===")
-        # print("\t\tTokens: {}".format(ex['tokens']))
+        if relation != RELATION_TEXT_MAPPING[R_INT_MAPPING[R]]:
+            continue
+        print("\n\t\t=== Extracted Relation ===")
+        print("\t\tInput Tokens: {}".format(ex['tokens']))
         subj = ex["subj"][0]
         obj = ex["obj"][0]
         confidence = pred[1]
-        # print("\t\tRelation: {} (Confidence: {:.3f})\nSubject: {}\tObject: {}".format(relation, confidence, subj, obj))
+        print("\t\tOutput Confidence: {:.6f})S ; Subject: {} ; Object: {}".format(confidence, subj, obj))
         if confidence > T:
             if res[(subj, relation, obj)] < confidence:
                 res[(subj, relation, obj)] = confidence
-                # print("\t\tAdding to set of extracted relations")
-    return res
+                print("\t\tAdding to set of extracted relations")
+            else:
+                print("\t\tDuplicate with lower confidence than existing record. Ignoring this.")
+        else:
+            print("\t\tConfidence is lower than threshold confidence. Ignoring this.")
+        print("\t\t==========\n")
+    return res, len(l)
 
 # TODO
 def get_openai_completion(prompt, model, max_tokens, temperature = 0.2, top_p = 1, frequency_penalty = 0, presence_penalty =0):
@@ -126,6 +122,8 @@ def get_openai_completion(prompt, model, max_tokens, temperature = 0.2, top_p = 
     return response_text
 
 def run_gpt3(sentence, valid_relation):
+    print("\n\t\t=== Extracted Relation ===")
+    print("\t\tSentence: {}".format(sentence))
 
     prompt_text = '''
                     Given a sentence, extract all the valid relations between nouns in tuple format (subject, relation, object).
@@ -163,9 +161,18 @@ def run_gpt3(sentence, valid_relation):
 
         relation = RELATION_TEXT_MAPPING.get(t[1].strip())
         new_tup = (t[0].strip(), relation, t[2].strip())
+
+        print("\t\tSubject: {} ; Object: {} ;".format(new_tup[0], new_tup[2]))
+
+        if result.get(new_tup):
+            print("\t\tDuplicate. Ignoring this.")
+            continue
+
+        print("\t\tAdding to set of extracted relations")
+        print("\t\t==========\n")
         result[new_tup] = 1.0
 
-    return result
+    return result, len(tuples)
 
 # extract the relations for a webpage via spanbert
 def extract(text, R, T, OPTION, spanbert_model, nlp, valid_relation):
@@ -174,12 +181,22 @@ def extract(text, R, T, OPTION, spanbert_model, nlp, valid_relation):
     if R in [1, 2, 4]:
         entities_of_interest = ["PERSON", "ORGANIZATION"]
     else:
-        entities_of_interest = ["PERSON", "Location", "CITY", "STATE_OR_PROVINCE", "COUNTRY"]
+        entities_of_interest = ["PERSON", "LOCATION", "CITY", "STATE_OR_PROVINCE", "COUNTRY"]
     
     # Split plaintext into sentences and extract relations
+    print('Annotating the webpage using spacy...')
     doc = nlp(text)
 
+    print(
+        '''
+        Extracted {} sentences. Processing each sentence one by one to check for presence of right pair of named entity types; 
+        if so, will run the second pipeline ...
+        '''.format(len(list(doc.sents)))
+        )
+
     # doc = nlp("Bill Gates stepped down as chairman of Microsoft in February 2014 and assumed a new post as technology adviser to support the newly appointed CEO Satya Nadella.")
+    extracted_count = 0
+    overall = 0
     for sent in doc.sents:
         # create entity pairs for each sentence
         ents = sp.create_entity_pairs(sent, entities_of_interest)
@@ -198,9 +215,9 @@ def extract(text, R, T, OPTION, spanbert_model, nlp, valid_relation):
                 elif ep[2][1] == 'PERSON' and ep[1][1] == 'ORGANIZATION':
                     examples.append({"tokens": ep[0], "subj": ep[2], "obj": ep[1]})
             elif R == 3:
-                if ep[1][1] == 'PERSON' and ep[2][1] in ["Location", "CITY", "STATE_OR_PROVINCE", "COUNTRY"]:
+                if ep[1][1] == 'PERSON' and ep[2][1] in ["LOCATION", "CITY", "STATE_OR_PROVINCE", "COUNTRY"]:
                     examples.append({"tokens": ep[0], "subj": ep[1], "obj": ep[2]})
-                elif ep[2][1] == 'PERSON' and ep[1][1] in ["Location", "CITY", "STATE_OR_PROVINCE", "COUNTRY"]:
+                elif ep[2][1] == 'PERSON' and ep[1][1] in ["LOCATION", "CITY", "STATE_OR_PROVINCE", "COUNTRY"]:
                     examples.append({"tokens": ep[0], "subj": ep[2], "obj": ep[1]})
             else:
                 if ep[1][1] == 'ORGANIZATION' and ep[2][1] == 'PERSON':
@@ -212,20 +229,24 @@ def extract(text, R, T, OPTION, spanbert_model, nlp, valid_relation):
 
         # if there are examples, find relations
         if examples:
+            extracted_count = extracted_count + 1
             if OPTION == '-spanbert':
-                r = run_spanbert(examples, T, spanbert_model)
+                r, total = run_spanbert(examples, T, spanbert_model, R)
                 res.update(r)
             else:
-                r = run_gpt3(sent.text, valid_relation)
+                r, total = run_gpt3(sent.text, valid_relation)
                 res.update(r)
                 time.sleep(1.5)
+            overall = overall + total
             
             # print('here')
             # print(examples)
         # otherwise go to next sentence
         else:
             continue
-
+    
+    print('Extracted annotations for  {}  out of total  {}  sentences'.format(extracted_count, len(list(doc.sents))))
+    print('Relations extracted from this website: {} (Overall: {})'.format(len(res.keys()), overall))
     return res
 
 def main():
@@ -261,49 +282,100 @@ def main():
     # Initialize URLS so we can see if a url has already been opened
     URLS = []
 
-    res = defaultdict(int) 
+    print(
+            '''
+            Parameters:
+            Client key      = {}
+            Engine key      = {}
+            OpenAI key      = {}
+            Method  = {}
+            Relation        = {}
+            Threshold       = {}
+            Query           = {}
+            # of Tuples     = {}
+            Loading necessary libraries; This should take a minute or so ...)
+            '''.format(
+                GOOGLE_API_KEY,
+                GOOGLE_ENGINE_ID,
+                OPEN_AI_KEY,
+                OPTION,
+                R_INT_MAPPING.get(R),
+                T,
+                Q,
+                K
+            )
+        )
 
-    # TODO: make sure this part goes in a loop
-    # Query your Google Custom Search Engine to obtain the URLs for the top-10 webpages for query q
-    # Make search API call
-    items = get_google_search_items(GOOGLE_API_KEY, GOOGLE_ENGINE_ID, Q)
+    iteration = 0
+    while True:
+        print('=========== Iteration: {} - Query: {} ==========='.format(iteration, Q))
+        iteration = iteration + 1
+        res = defaultdict(int) 
 
-    # Format items to desired output
-    output = get_formatted_items(items)
-    
-    # Initialize spacy
-    nlp = spacy.load("en_core_web_lg")
+        # Query your Google Custom Search Engine to obtain the URLs for the top-10 webpages for query q
+        # Make search API call
+        items = get_google_search_items(GOOGLE_API_KEY, GOOGLE_ENGINE_ID, Q)
 
-    if OPTION == '-spanbert':
-        # Load spanbert model
-        spanbert_model = span.SpanBERT("./pretrained_spanbert")
-    else:
-        spanbert_model = 0
+        # Format items to desired output
+        output = get_formatted_items(items)
+        
+        # Initialize spacy
+        nlp = spacy.load("en_core_web_lg")
 
-    # Get text from each url
-    for doc in output:
-        url = doc['url']
-        # If url already seen, continue
-        if url in URLS:
-            continue
+        if OPTION == '-spanbert':
+            # Load spanbert model
+            spanbert_model = span.SpanBERT("./pretrained_spanbert")
         else:
-            # add url to URLS and get 10000 chars of text
-            URLS.append(url)
-            
-            # print('1')
-            plaintext = get_website(url)
-            # print('2')
-            # if url cannot be opened, go to next one
-            if plaintext == 0:
+            spanbert_model = 0
+
+        # Get text from each url
+        url_num = 1
+        for doc in output:
+            print('URL ( {} / {}): {}'.format(url_num, len(output), doc))
+            print('Fetching text from url ...')
+            url = doc['url']
+
+            url_num = url_num + 1
+
+            # If url already seen, continue
+            if url in URLS:
                 continue
-            if len(plaintext) > 10000:
-                plaintext = plaintext[:10000]
-            # split text into sentences and extract entities
-            relations = extract(plaintext, R, T, OPTION, spanbert_model, nlp, R_INT_MAPPING.get(R))
-            res.update(relations)
-            # print('3')
-    print(res)
-            
+            else:
+                # add url to URLS and get 10000 chars of text
+                URLS.append(url)
+                
+                # print('1')
+                plaintext = get_website(url)
+                # print('2')
+                # if url cannot be opened, go to next one
+                if plaintext == 0:
+                    continue
+                if len(plaintext) > 10000:
+                    print('Trimming webpage content from {} to 10000 characters'.format(len(plaintext)))
+                    plaintext = plaintext[:10000]
+
+                print('Webpage length (num characters): {}'.format(len(plaintext)))
+
+                # split text into sentences and extract entities
+                relations = extract(plaintext, R, T, OPTION, spanbert_model, nlp, R_INT_MAPPING.get(R))
+                res.update(relations)
+
+        # if res has at least K tuples, stop and return
+        sorted_res = sorted(res.items(), key=lambda x:x[1])
+        if len(res.items()) >= K or len(res.items()) == 0:
+            break
+        
+        # Else continue loop
+        # Take first item of dict and use as query
+        top_tup = next(iter(sorted_res))
+        Q = ' '.join(top_tup)
+
+
+    print('================== ALL RELATIONS for per:cities_of_residence ( 10 ) =================')
+
+    for tup, confidence in res.items():
+        print('Confidence: {}            | Subject: {}              | Object: {}'.format(confidence, tup[0], tup[2]))
+    print('Total # of iterations = {}'.format(iteration))
     return 0
 
 if __name__ == "__main__":
